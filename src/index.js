6b5a42ad7769907885152ec94b377b1acbb83d65
lchieng2 <lchieng15@gmail.com>
2022-09-27 12:09:42 -0500

    initial mp1 changes

--- src/index.js
+++ src/index.js
@@ -11,3 +11,43 @@ import './css/main.scss';
 
 // Scripts
 import './js/main.js';
+
+
+
+
+
+// Position indicator
+const sections = document.querySelectorAll('section[id]')
+
+window.addEventListener('scroll', scrollActive)
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 20;
+        const sectionId = current.getAttribute('id')
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('a[href*=' + sectionId + ']').classList.add('active')
+        } else {
+            document.querySelector('a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+
+// Sticky scrolling function
+const nav = document.querySelector('nav');
+
+let navTop = nav.offsetTop;
+
+function fixedNav() {
+  if (window.scrollY >= navTop) {    
+    nav.classList.add('fixed');
+  } else {
+    nav.classList.remove('fixed');    
+  }
+}
+
+window.addEventListener('scroll', fixedNav);
